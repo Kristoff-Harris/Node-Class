571334b8baba6bf46fdcd6fdd7374bbbb6d5344f
Chris Harris <cjh6470@rit.edu>
2017-09-13 19:57:14 -0400

    Create client.js

---
+++ client.js
@@ -0,0 +1,9 @@
+// Listen for command in a loop and send them to the server
+
+// initial connection
+
+//lookupByLastName Smith
+//addEmployee William Smith
+//lookupByLastName Smith
+// Disconnect with a 'bye'
+
